763a6044c1d3aecb6c58b5e32ea313cfbe7db659
ryan <ryanwaszak@gmail.com>
2020-03-18 09:47:17 -0500

    Add bcrypt authentication with users controller, users#create, sessions#create, and users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
+    get "/users" => "users#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+  end
+  
 end
